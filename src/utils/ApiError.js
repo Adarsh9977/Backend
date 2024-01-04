6f814c232687de214f76e7746ec4e58a41400f06
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2024-01-04 13:36:20 +0530

    multer and jwt added

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
         statusCode,
         message= "something went wrong",
         Errors = [],
-        statck = "",
+        stack = "",
     ){
         super(message)
         this.statusCode = statusCode
@@ -12,8 +12,8 @@ class ApiError extends Error {
         this.success = false;
         this.errors = Errors
 
-        if(statck){
-            this.stack = statck
+        if(stack){
+            this.stack = stack
         }
         else{
             Error.captureStackTrace(this, this.constructor)
