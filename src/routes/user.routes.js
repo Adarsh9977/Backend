a3dcfbf7ac76c4357b07ab724fe899bde4e2e2fc
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2024-01-05 15:14:13 +0530

    Add register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import registerUser from "../controllers/user.controller.js"
 const router = Router(); 
+import {upload} from "../middlewares/multer.middleware.js"
 
-
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name : "avatar",
+            maxCount :1
+        },
+        {
+            name:"coverImage",
+            maxCount : 1
+        }
+    ]),
+    registerUser)
 
 export default router;
