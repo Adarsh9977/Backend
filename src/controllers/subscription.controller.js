58a9a37f8a53e154317b222f547e74d73e942a57
Adarsh Tiwari <adarshtiwari797023@gmail.com>
2024-01-14 01:26:15 +0530

    write subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -44,11 +44,55 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    if (!channelId) {
+        throw new ApiError(400, "channelId id required")
+    }
+    const subscribers = await Subscription.aggregate([
+        {
+            $match:{
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $project:{
+                username:1,
+                fullname:1,
+                avatar:1
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, subscribers, "subscribers list "))
+
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    if (!subscriberId) {
+        throw new ApiError(400, "subscriberId is required")    
+    }
+
+    const channelList = await Subscription.aggregate([
+        {
+            $match:{
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $project:{
+                username:1,
+                fullname:1,
+                avatar:1,
+            }
+        }
+    ])
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channelList, "get channel lit successfully"))
 })
 
 export {
